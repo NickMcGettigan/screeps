8dbd71b22307a64638e92dca912ebd2a95db6e3e
Nick McGettigan <nick.mcgettigan@gmail.com>
2016-11-23 00:20:13 -0800

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -13,83 +13,84 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
-
+        
         if (creep.memory.energystate == 'empty'){
             var sources = creep.room.find(FIND_SOURCES);
             if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         }
+        
+        
+        
+        if (creep.memory.energystate == 'full'){
+            // find closest spawn or extension which is not full
+            var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+                // the second argument for findClosestByPath is an object which takes
+                // a property called filter which can be a function
+                // we use the arrow operator to define it
+                filter: (s) => s.energy < s.energyCapacity
+            });
 
-        if(creep.memory.energystate == 'full'){
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                        filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                    structure.structureType == STRUCTURE_SPAWN ||
-                    structure.structureType == STRUCTURE_CONTAINER ||
-                    structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+            // if we found one
+            if (structure != undefined) {
+                // try to transfer energy, if it is not in range
+                if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    // move towards it
+                    creep.moveTo(structure);
+                }
+            }
+            
+            
+            
+            
+            
+            
+        }      
+ /*       
+        
+        
+        if (creep.carry.energy < 50 ) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
         }
-        });
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                            structure.structureType == STRUCTURE_SPAWN ||
+                            structure.structureType == STRUCTURE_CONTAINER ||
+                            structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                }
+            });
             if (targets.length > 0) {
                 if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
             }
-
-
+        
+            
             if( Game.spawns['Spawn1'].energy >= 300){
                 if (creep.carry.energy > 0 ) {
                     if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(creep.room.controller);
-                    }
+                    }    
                 }
             }
-
-
+            */
+            
         }
-        /*
-
-
-         if (creep.carry.energy < 50 ) {
-         var sources = creep.room.find(FIND_SOURCES);
-         if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-         creep.moveTo(sources[0]);
-         }
-         }
-         else {
-         var targets = creep.room.find(FIND_STRUCTURES, {
-         filter: (structure) => {
-         return (structure.structureType == STRUCTURE_EXTENSION ||
-         structure.structureType == STRUCTURE_SPAWN ||
-         structure.structureType == STRUCTURE_CONTAINER ||
-         structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-         }
-         });
-         if (targets.length > 0) {
-         if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-         creep.moveTo(targets[0]);
-         }
-         }
-
-
-         if( Game.spawns['Spawn1'].energy >= 300){
-         if (creep.carry.energy > 0 ) {
-         if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-         creep.moveTo(creep.room.controller);
-         }
-         }
-         }
-         */
-
+    
+            
+            
     }
-
-
-
-}
-
-
+    
+        
 
 
 
 module.exports = roleHarvester;
+
 
